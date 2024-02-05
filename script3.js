b4f457e6de266a955b9e260db2be6afd5ac50dbf
Dinakaran-FSD-Developer <123656660+dinadina1@users.noreply.github.com>
2024-02-05 19:33:26 +0530

    map method

--- script3.js
+++ script3.js
@@ -1,13 +1,11 @@
 // Solving problems using array functions on rest countries data (https://restcountries.com/v3.1/all).
 // Question 1 : d) Print the total population of countries using reduce function
 
-var req = new XMLHttpRequest();
-req.open("GET","https://restcountries.com/v3.1/all");
+const req = new XMLHttpRequest();
+req.open("GET", "https://restcountries.com/v3.1/all");
 req.send();
-req.onload = function(){
-    var result=JSON.parse(req.response);
-    var fill=result.map((val)=>val.population)
-    var total=fill.reduce((acc,sum)=>acc+sum);
-    console.log("Total Population of all Countries");
-    console.log(total);
+req.onload=()=>{
+    let res=JSON.parse(req.response);
+    let population=res.reduce((acc,ele)=>acc+ele.population,0)
+    console.log(`Total Population for all Countries : ${population.toLocaleString()}`);
 }
