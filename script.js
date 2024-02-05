d3ffd4c5373800213ea88729b6a0443f2943b786
Dinakaran-FSD-Developer <123656660+dinadina1@users.noreply.github.com>
2024-02-05 12:06:22 +0530

    map method

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 // Solving problems using array functions on rest countries data (https://restcountries.com/v3.1/all).
 // Question No 1 : a) Get all the countries from Asia continent /region using Filter function
 
-var req = new XMLHttpRequest();
-req.open("GET","https://restcountries.com/v3.1/all");
+const req = new XMLHttpRequest();
+req.open("GET", "https://restcountries.com/v3.1/all");
 req.send();
-req.onload = function(){
-    var result =JSON.parse(req.response);
-    var output=result.filter((value)=>value.continents=="Asia" && value.region=="Asia");
-    var countrylist=output.map((country)=>country["name"]["common"]);
-    console.log("Asian Countries Name List...")
-    for(let i=0;i<countrylist.length;i++){
-        console.log(`${i+1}) ${countrylist[i]}`);
-    }
-}
+req.onload = () => {
+  let res = JSON.parse(req.response);
+  console.log("Asia Continents/Region Countries List:");
+  let country = res.filter((data) => data.region === "Asia" || data.continents[0] === "Asia");
+  country.map((data) => console.log(`Country Name : ${data.name.common}`));
+};
