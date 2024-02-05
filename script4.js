f9acbf87b9e27ca87d8fae5a44273607e5521b8f
Dinakaran-FSD-Developer <123656660+dinadina1@users.noreply.github.com>
2024-02-05 19:49:52 +0530

    map method

--- script4.js
+++ script4.js
@@ -1,15 +1,15 @@
 // Solving problems using array functions on rest countries data (https://restcountries.com/v3.1/all).
 // Question No 1 : e) Print the country that uses US dollars as currency.
 
-var req = new XMLHttpRequest();
-req.open("GET","https://restcountries.com/v3.1/all");
+const req = new XMLHttpRequest();
+req.open("GET", "https://restcountries.com/v3.1/all");
 req.send();
-req.onload = function(){
-    var result =JSON.parse(req.response);  
-    console.log("These countries are using US dollar...");  
-    for(var i=0;i<result.length;i++){
-        if(result[i].currencies.USD){
-            console.log(`Country Name : ${result[i].name.common}`);
+req.onload=()=>{
+    let res=JSON.parse(req.response);
+    console.log("These Countries using US Dollors..")
+    res.forEach((element,i) => {
+        if(element.currencies.USD){
+            console.log(`Country Name: ${element.name.common}`);
         }
-    }
+    });
 }
