72ba8eca3f418bd0287eb4a0d6f9621d754dc0ba
Dinakaran-FSD-Developer <123656660+dinadina1@users.noreply.github.com>
2024-02-05 19:29:53 +0530

    map method

--- script2.js
+++ script2.js
@@ -1,13 +1,13 @@
 // Solving problems using array functions on rest countries data (https://restcountries.com/v3.1/all).
 // Question No 1 : c) Print the following details name, capital, flag, using forEach function
 
-var req=new XMLHttpRequest();
-req.open("GET","https://restcountries.com/v3.1/all");
+const req = new XMLHttpRequest();
+req.open("GET", "https://restcountries.com/v3.1/all");
 req.send();
-req.onload=function(){
-    var result=JSON.parse(req.response);    
-    console.log("All the Countries Name, Capital and Flag List..")  
-    result.forEach((info,i)=>
-    console.log(`Country Name : ${info.name.common}\nCapital : ${info.capital}\nFlag : ${info.flags.svg}\n`,i+1)
-    );
-}
+req.onload = () => {
+  let res = JSON.parse(req.response);
+  console.log("All the Countries Name, Capital and Flag List..")
+  res.forEach((element,i) => {
+        console.log(`${i+1})  Country Name: ${element.name.common} \n Capital: ${element.capital} \n Flag: ${element.flags.png}`);
+  });
+};
