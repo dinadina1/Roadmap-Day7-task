7e69ca039b04521d0a9ff5b769f2dc630b66b8c8
Dinakaran-FSD-Developer <123656660+dinadina1@users.noreply.github.com>
2024-02-05 19:12:20 +0530

    map method

--- script1.js
+++ script1.js
@@ -1,15 +1,12 @@
 // Solving problems using array functions on rest countries data (https://restcountries.com/v3.1/all).
 //Question 1 : b) Get all the countries with a population of less than 2 lakhs using Filter function
 
-var req = new XMLHttpRequest();
-req.open("GET","https://restcountries.com/v3.1/all");
+const req = new XMLHttpRequest();
+req.open("GET", "https://restcountries.com/v3.1/all");
 req.send();
-req.onload = function(){
-    var result =JSON.parse(req.response);
-    var output=result.filter((value)=>value.population<=200000);
-    var countrylist=output.map((country)=>country["name"]["common"]);
-    console.log("These Countries Population is less than 2 lakhs...")
-    for(let i=0;i<countrylist.length;i++){
-        console.log(`${i+1}) ${countrylist[i]}`);
-    }
-}
+req.onload = () => {
+  let res = JSON.parse(req.response);
+  console.log("Countries List with less than 2Lakh Population");
+  let population=res.filter((ele)=>ele.population < 200000)
+  let popul_name = population.map((ele)=>console.log(`${ele.name.common} : ${ele.population.toLocaleString()}`))
+};
